3f7f22dd4fe1ce949b4286e0c26c09790a0b7d6a
santipu <santipujolsau@gmail.com>
2022-12-09 12:30:46 +0100

    created function in server to recover a public key from a signed msg

--- server/index.js
+++ server/index.js
@@ -2,20 +2,43 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3042;
+const secp = require("ethereum-cryptography/secp256k1");
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
 
 app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "0xe34655d1af60239111125ba68377f6731f83f54d": 100,
+  "0x83a54340b036b2679cef2b300b1178e33033f67e": 50,
+  "0x82850e255b1b6dc6ca2014da9731b5cdbf3b40b3": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
   const { address } = req.params;
   const balance = balances[address] || 0;
   res.send({ balance });
+});
+
+app.post("/verify", (req, res) => {
+  const { msgHash, signature, recoveryBit } = req.body;
+  const recoverPublicKey = async (msgHash, signature, recoveryBit) => {
+    const recoveredPublicKey = await secp.recoverPublicKey(
+      msgHash,
+      signature,
+      recoveryBit
+    );
+    return recoveredPublicKey;
+  };
+
+  recoverPublicKey(msgHash, signature, parseInt(recoveryBit)).then(
+    (recoveredPublicKey) => {
+      res.send({
+        recoveredPublicKey: toHex(recoveredPublicKey),
+      });
+    }
+  );
 });
 
 app.post("/send", (req, res) => {
