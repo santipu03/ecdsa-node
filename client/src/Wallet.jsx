054cde5f6c68da549df96dc9d79cab13173dff5f
santipu <santipujolsau@gmail.com>
2022-12-09 14:30:27 +0100

    modified Wallet component to only show balances of a wallet

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,22 +1,9 @@
 import server from "./server";
-import * as secp from "ethereum-cryptography/secp256k1";
-import { toHex } from "ethereum-cryptography/utils";
-import { keccak256 } from "ethereum-cryptography/keccak";
-import { useState } from "react";
 
-function Wallet({
-  address,
-  setAddress,
-  balance,
-  setBalance,
-  signedMessage,
-  setSignedMessage,
-}) {
-  const { status, setStatus } = false;
-
+function Wallet({ address, setAddress, balance, setBalance }) {
   async function onChange(evt) {
-    const signedMessage = evt.target.value;
-    setSignedMessage(signedMessage);
+    const address = evt.target.value.toLowerCase();
+    setAddress(address);
   }
 
   async function checkBalance() {
@@ -30,49 +17,29 @@ function Wallet({
     }
   }
 
-  // async function verifyMessage() {
-  //   if (signedMessage) {
-  //     const {
-  //       data: { balance },
-  //     } = await server.get(`balance/${address}`);
-  //     setBalance(balance);
-  //   } else {
-  //     setBalance(0);
-  //   }
-  // }
-
-  async function verifyMessage() {
-    if (signedMessage) {
-      const {
-        data: { msg },
-      } = await server.post(`verify`, {
-        msg: signedMessage,
-      });
-      console.log(msg);
-    }
-  }
-
   return (
     <div className="container wallet">
-      <h1>Wallet</h1>
-
+      <h1>Your Wallet</h1>
       <label>
-        Signed Message
+        Wallet Address
         <input
-          placeholder="Paste the signed message here"
-          value={signedMessage}
+          placeholder="Type an address"
+          value={address}
           onChange={onChange}
         ></input>
       </label>
-
-      {status ? <div>Status: VERIFIED</div> : <div>Status: NOT VERIFIED</div>}
-
-      <button onClick={verifyMessage} className="button">
-        Verify Message
-      </button>
-
-      <div className="balance">Wallet: {address}</div>
+      <div type="button" className="button" onClick={checkBalance}>
+        Check Balance
+      </div>
       <div className="balance">Balance: {balance}</div>
+      <div className="balance">
+        Example Wallets:
+        <ul>
+          <li>0xe34655d1af60239111125ba68377f6731f83f54d</li>
+          <li>0x83a54340b036b2679cef2b300b1178e33033f67e</li>
+          <li>0x82850e255b1b6dc6ca2014da9731b5cdbf3b40b3</li>
+        </ul>
+      </div>
     </div>
   );
 }
